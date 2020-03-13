4952c16a64ccd6fdba7f5b04dc3c4b66c320d58e
mokhtarbensaid <mokhtar1992bensaid@gmail.com>
2020-03-13 11:20:54 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$( function () {
+   'use strict';
+   $(window).scroll(function(){
+       var navbar = $('.navbar');
+       if($(window).scrollTop() >= navbar.height()){
+        navbar.addClass('scrolled');
+       }else{
+        navbar.removeClass('scrolled');
+       }
+   });
+      $('.nav-item').click(function () {
+     $(this).addClass('active').siblings().removeClass('active');
+   });
+
+});
